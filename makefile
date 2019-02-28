33f1e94876b3829b327157b793279a31a104b3de
David Phillips <phillida@tcd.ie>
2019-02-28 17:31:22 +0000

    Add oisin's grid search.

--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 sol:
 	@echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n-------------'
-	g++ -std=c++11 -o sol sol.cpp
+	g++ -std=c++14 -fopenmp -lpthread -o sol sol.cpp
--- sol.cpp
+++ sol.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include <dirent.h>
+#include <omp.h>
 
 #define ii pair<int,int>
 #define vi vector<int>
@@ -24,6 +25,13 @@
 #define fr(i,n) for (int i = 0; i < n; i++)
 
 using namespace std;
+
+struct bound {
+    string name;
+    float start;
+    float end;
+    int steps;
+};
 
 int bestScore = INT_MIN;
 
@@ -71,4 +79,123 @@ int main(int argc, char* argv[]) {
 	}
 
 	cerr << "Best score before starting: " << bestScore << endl;
+
+	
+
+	// Bounds
+	vector<bound> bs;
+	bs.push_back({"t1", 0, 5, 5});
+	bs.push_back({"t2", 1, 100, 5});
+	// bs.push_back({"t3", 3.2, 34, 8});
+
+	// Grid size
+	for (int x = 0; x < 5; x++) {
+
+		auto start = std::chrono::steady_clock::now();
+
+		// Print bounds
+		cerr << "bounds:" << endl;
+		for (auto b : bs) {
+			cerr << b.name << " start: " << b.start << " end: " << b.end << " steps: " << b.steps << endl;
+		}
+
+		// Make vectors
+		vector<vector<float>> config(1);
+		for (auto b : bs) {
+
+
+			vector<vector<float>> config_tmp;
+			for (int i = 0; i < b.steps; i++) {
+				float v = b.start + i*(b.end - b.start)/(b.steps-1);
+
+				// Add this val to every element in config already.
+				for (int i = 0; i < config.size(); i++) {
+					vector<float> c = config[i];
+					c.push_back(v);
+					config_tmp.push_back(c);
+				}
+
+			}
+			config = config_tmp;
+		}
+
+
+		// test
+		// for (auto v : config) {
+		//     for (int i = 0; i < v.size(); i++) {
+		//         cerr << bs[i].name << ": " << v[i] << ", ";
+		//     }
+
+		//     cerr << endl;
+		// }
+
+		// Lock
+		omp_lock_t writelock;
+		omp_init_lock(&writelock);
+
+		float best_score = 0;
+		vector<float> best_params;
+
+#pragma omp parallel for
+		for (int i = 0; i < config.size(); i++) {
+			auto begin = std::chrono::steady_clock::now();
+			vector<float> v = config[i];
+
+
+
+
+
+
+
+
+
+			// TODO: Solve problem
+			float score = 10000 - (v[0] - 3.5)*(v[0] - 3.5) - (v[1] - 60)*(v[1] - 60);
+
+
+
+
+
+
+
+			// output...
+			omp_set_lock(&writelock);
+
+			cerr << "score: " << score << "       ";
+
+			for (int i = 0; i < v.size(); i++) {
+				cerr << bs[i].name << ": " << v[i] << ", ";
+			}
+
+			if (score > best_score) {
+				// TODO: save file
+				best_params = v;
+				best_score = score;
+			}
+
+			std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+			float row   = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.0;
+			cerr << " took: " << row;
+
+			cerr << endl;
+			omp_unset_lock(&writelock);
+		}
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		float total = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000000.0;
+		cerr << "Completed grid!! Time: " << total << " score: " << best_score << endl;
+		cerr << "Best params: ";
+		for (float v : best_params)
+			cerr << " " << v;
+		cerr << endl << endl;
+
+		// TODO: Check this is right lul
+		for (int i = 0; i < bs.size(); i++) {
+			auto p = best_params[i];
+			float inc = (bs[i].end - bs[i].start)/(bs[i].steps-1);
+
+			bs[i].start = p-inc;
+			bs[i].end = p+inc;
+		}
+
+	}
 }
